14dbd0daf5240b8f042a646b04cc770aa55d15e7
ferret <73801176+ferretirl@users.noreply.github.com>
2026-06-20 15:59:43 +0200

    credential storing in git

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -5,6 +5,7 @@
       settings = {
           user.name = "Ferret";
           user.email = "73801176+ferretirl@users.noreply.github.com";
+          credential.helper = "store";
         };
     };
   };
