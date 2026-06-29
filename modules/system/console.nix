ac0111df03efbd74dc4f1e0594e2de42dfb1937c
Alice <73801176+AliceOnPenguins@users.noreply.github.com>
2026-06-29 21:16:00 +0200

    fix tty font not applying itself correctly

--- modules/system/console.nix
+++ modules/system/console.nix
@@ -1,7 +1,8 @@
 { self, inputs, ... }: {
     flake.nixosModules.consoleSetup = { pkgs, lib, ... }: {
         console = {
-            font = "${pkgs.terminus_font}/share/consolefonts/ter-132b.psf.gz";
+            font = "ter-132b";
+            packages = [ pkgs.terminus_font ];
             keyMap = "us";
           };
       };
